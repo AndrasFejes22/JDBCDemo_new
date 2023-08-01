820bd437fba9c2b89b1d5298cb6f3096b51cae11
AndrasFejes22 <afejes22@gmail.com>
2023-08-01 15:32:33 +0200

    txt files added

---
+++ src/main/java/jdbcdemo/initdb/InitDb.java
@@ -0,0 +1,11 @@
+package jdbcdemo.initdb;
+
+/**
+ * Write a Java program that randomly fills the tables of the "webshop" database with sample data.
+ * The program should be parameterizable (e.g. how many records are inserted into which table.)
+ */
+public class InitDb {
+    public static void main(String[] args) {
+
+    }
+}
